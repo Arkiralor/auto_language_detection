d06e17e352339c6680b6122efbcdf2f9b09aa421
Arkiralor <prithoo11335@gmail.com>
2022-03-11 01:09:49 +0530

    def arg changes.

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ Python program to automatically detect the Natural Language of a given text-docu
 from code.functions import check_language
 
 if __name__ == "__main__":
-    file_name: str = "sa_01.txt"
+    file_name: str = "en_01.txt"
     check_language(file_name=file_name)

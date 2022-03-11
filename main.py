c670af0e78204af0d425e98a9a70b02dceb6fdd6
Arkiralor <pritho11335@gmail.com>
2022-03-11 12:24:04 +0530

    added system args

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 '''
 Python program to automatically detect the Natural Language of a given text-document.
 '''
-
+import sys
 from code.functions import check_language
 
+def main():
+    input_args = sys.argv
+    try:
+        if len(input_args) == 2:
+            file_path = input_args[1]
+            check_language(file_path)
+        else:
+            print("Please provide a file path as an argument.")
+    except Exception as e:
+        print(f"Error in {__name__}: {e}")
+
 if __name__ == "__main__":
-    file_name: str = "en_01.txt"
-    check_language(file_name=file_name)
+    main()
